621ff8cec0ba7e5f6be6ea51ba8899260908a0e0
Canyon Smith <canyonfsmith@gmail.com>
2023-02-04 19:04:52 -0700

    made file paths based on pathlib

--- scripts/utils/get_values.py
+++ scripts/utils/get_values.py
@@ -1,38 +1,12 @@
 import sys
-sys.path.append("../..")  # Adds higher directory to python modules path.
+import os
+from pathlib import Path
+
 import json
 from scripts.utils.openai_secret_manager import (
     OpenAiSecretManager as openai_secret_manager,
 )
 import openai
-
-def get_background_information(info_name):
-    """
-    Open the background information file and
-    return the info_name
-    :param info_name: the name of the information
-    :return: a list
-    """
-    return list(
-        _get_data_from_file(
-            "files/data/examples/background_information.jsonl"
-        )[info_name].values()
-    )  # Get the values of the info_name
-
-
-def get_hooks_examples_from_file():
-    """
-    Open the hook examples file and
-    return the hooks
-    :return: a string of hooks
-    """
-    return "\n".join(
-        json.loads(line)["hook"]
-        for line in open(
-            "files/data/examples/hook_examples.jsonl",
-            "r",
-        )
-    )  # Get the hook of each line
 
 
 def _get_data_from_file(file_name):
@@ -44,19 +18,8 @@ def _get_data_from_file(file_name):
     return json.load(open(file_name))
 
 
-def append_key_value_to_temp_json_file(key, values):
-    temp_file = "../../files/data/temp/temp.jsonl"
-    with open(temp_file, "r") as f:
-        steps = [json.loads(line) for line in f]
-    for i, step in enumerate(steps):
-        step[key] = values[i]
-    with open(temp_file, "w") as f:
-        for step in steps:
-            f.write(json.dumps(step) + "\n")
-
-
 def get_key_values_from_temp_json_file(key):
-    temp_file = "../../files/data/temp/temp.jsonl"
+    temp_file = get_file_path("files/data/examples/temp/temp.jsonl")
     with open(temp_file, "r") as f:
         values = [json.loads(line)[key] for line in f]
     return values
@@ -74,3 +37,47 @@ def _openai_response(prompt):
         presence_penalty=0,
     )
     return response["choices"][0]["text"]
+
+def get_file_path(file_name):
+    """
+    Get the file path of a file
+    :param file_name: the name of the file
+    :return: the path of the file
+    """
+    return os.path.join(Path(__file__).parent.parent.parent, file_name)
+
+
+def get_background_information(info_name):
+    """
+    Open the background information file and
+    return the info_name
+    :param info_name: the name of the information
+    :return: a list
+    """
+    return _get_data_from_file(
+        get_file_path("files/data/examples/background_information.jsonl")
+    )[info_name]
+
+def get_hooks_examples_from_file():
+    """
+    Open the hook examples file and
+    return the hooks
+    :return: a string of hooks
+    """
+    print(os.getcwd())
+    return "\n".join(
+        json.loads(line)["hook"]
+        for line in open(
+            get_file_path("files/data/examples/hook_examples.jsonl"), "r"
+        )
+    )
+
+def append_key_value_to_temp_json_file(key, values):
+    temp_file = get_file_path("files/data/examples/temp/temp.jsonl")
+    with open(temp_file, "r") as f:
+        steps = [json.loads(line) for line in f]
+    for i, step in enumerate(steps):
+        step[key] = values[i]
+    with open(temp_file, "w") as f:
+        for step in steps:
+            f.write(json.dumps(step) + "\n")
