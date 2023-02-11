1fe213c1638a1525842efe6de5d0d75a1aa565e9
Canyon Smith <canyonfsmith@gmail.com>
2023-02-10 20:53:21 -0700

    converted final html bug fix

--- scripts/convert_final_to_html_and_css.py
+++ scripts/convert_final_to_html_and_css.py
@@ -4,11 +4,13 @@ import sys
 import json
 import pathlib
 import webbrowser
+
+
+FILE_PATH = "/Users/canyons/Documents/GitHub/mairkeeteer/files/data/examples/html_css_example_email.jsonl"
 import openai
 
 
 def openai_complete(prompt):
-
     all_text = pathlib.Path(
         "../../../files/data/examples/html_css_example_email.jsonl"
     ).read_text()
@@ -68,8 +70,6 @@ def render_html_in_browser(html):
     html_css_file = HTMLTemplateFile(
         "/Users/canyons/Documents/GitHub/mairkeeteer/files/data/examples/html_css_example_email.jsonl"
     )
-    # print(html_css_file.get_code_examples())
-    # print(html_css_file.get_email_examples())
     html_css_file.render_html_in_browser(str(html_css_file.get_code_examples()[-1]))
     print("rendered")
 
@@ -90,9 +90,6 @@ def openai_complete(prompt, last_line):
     all_text = pathlib.Path(
         "/Users/bottega/Desktop/mairket/mairkeeteer/files/data/examples/html_css_example.jsonl"
     ).read_text()
-
-    # get the value of the 'Email' key of the last line
-    # all_text = json.loads(all_text)["Email"]
     prompt = (
         f"""Example of converting Email into HTML/CSS\nEmail:\nSubject: Unock Your Course Creation Potential with Course Creator Pro\n\n\\Dear [Name],\n\n\n\Are you looking to create and launch your own successful online course? If so, I have the perfect solution for you\n\n\nIntroducing Course Creator Pro – a comprehensive course that teaches you everything you need to know about creating and launching your own online course. With our step-by-step guide, templates, and resources, you’ll be able to get started quickly and easily.\n\n\nLearn how to create an engaging course that will attract students and generate revenue. Get access to our exclusive resources and learn from our experienced instructors. With Course Creator Pro, you’ll have all the tools you need to become a successful online course creator.\n\n\nLearn more about Course Creator Pro today and unlock your course creation potential.\n\n\nSincerely, \n\n\n[Your Name]\nCode:\n<!DOCTYPE html> <html> <head> <style> .container max-width: 600px;margin: 0 auto; padding: 20px; text-align: center; font-family: Arial, sans-serif;  h2  font-size: 26px; font-weight: bold; margin-bottom: 20px;  p  font-size: 16px; line-height: 1.5; margin-bottom: 20px; a display: inline-block; padding: 10px 20px; background-color: #2ab27b; color: #fff; text-decoration: none; border-radius: 5px; margin-top: 20px; </style> </head> <body> <div class=\"container\"> <h2>Unlock Your Course Creation Potential with Course Creator Pro</h2> <p>Dear [Name],</p><p>Are you looking to create and launch your own successful online course? If so, I have the perfect solution for you. </p> <p>Introducing Course Creator Pro – a comprehensive course that teaches you everything you need to know about creating and launching your own online course. With our step-by-step guide, templates, and resources, you’ll be able to get started quickly and easily. </p> <p>Learn how to create an engaging course that will attract students and generate revenue. Get access to our exclusive resources and learn from our experienced instructors. With Course Creator Pro, you’ll have all the tools you need to become a successful online course creator.</p> <a href=\#\>Learn More Today</a> </div></body> </html>\n\n\nNew Email\n{last_line}\n\n\nNew Code\n""",
     )
@@ -163,10 +160,3 @@ def get_last_line_of_file(file_path):
     with open(file_path, "r") as f:
         last_line = f.readlines()[-1]
     return last_line
-
-
-print(
-    get_last_line_of_file(
-        "/Users/bottega/Desktop/mairket/mairkeeteer/files/data/examples/html_css_example.jsonl"
-    )
-)
